1998e070258d9a511fd202a386a09d23c2ca4b42
Jeremieon <115072636+Jeremieon@users.noreply.github.com>
2023-08-30 01:34:06 +0200

    unique class

--- cars_app/serializers.py
+++ cars_app/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import Vehicle,Category
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','name']
 
-class VehicleSerializer(serializers.HyperlinkedModelSerializer):
+class VehicleSerializer(serializers.ModelSerializer):
+    category = serializers.ReadOnlyField(source='category.name')
     class Meta:
         model = Vehicle
         fields = ['id','make','model','year','category','price_usd','country_made','wheels','horsepower','weight','color','fuel_type']
